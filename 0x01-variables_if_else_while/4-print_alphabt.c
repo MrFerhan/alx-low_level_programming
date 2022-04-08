f47fcc0dfc1fafcdbb672555a09aed9dfc312909
MrFerhan <ferhkem@gmail.com>
2022-04-08 16:30:34 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+/**main - start
+ *
+ * Return: Aways 0
+ *
+ */
+
+int main(void)
+
+{char ch = 'a';
+
+
+
+	while (ch <= 'z')
+
+	{
+
+		if (ch != 'e' && ch != 'q')
+
+		{
+
+			putchar(ch);
+
+		}
+
+		ch++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

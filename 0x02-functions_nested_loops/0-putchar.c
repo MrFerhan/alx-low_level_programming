cae5ad47febf26fddc85eb205c9671ad104ee6d9
MrFerhan <ferhkem@gmail.com>
2022-04-12 14:10:20 -0700

    added put char.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * return: always sucess
+ */
+int _putchar(char c)
+{
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
+
+}

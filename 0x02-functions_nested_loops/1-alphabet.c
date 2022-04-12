c18ff940fdb495f0067ce4acde9243351a979e51
MrFerhan <ferhkem@gmail.com>
2022-04-12 15:51:09 -0700

    task 1 print alphabet usiing putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line
+ *
+ * Return: always sucess
+ */
+
+void print_alphabet(void);
+{
+	char x;
+	
+	for(x = 'a';x <= 'z';x++)
+	{
+		_putchar('x');
+	}
+	_putchar('\n');
+
+	return (0);
+
+}

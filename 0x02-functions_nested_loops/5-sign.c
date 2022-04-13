906e6a39b5ca54002c4a614bd4b45c6970304055
MrFerhan <ferhkem@gmail.com>
2022-04-12 18:19:20 -0700

    task 5greater lessthan

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,16 @@ int print_sign(int n)
 	if (n > 0 )
 	{
 		return (1);
-		putchar ('+');
+		putchar('+');
 	}
 	else if (n == 0)
 	{
 		return (0);
-		putchar ('0');
+		putchar('0');
+	}
 	else (n < 0)
 	{
 		return (-1);
-		putchar ('-');
-	}
+		putchar('-');
 	}
 }

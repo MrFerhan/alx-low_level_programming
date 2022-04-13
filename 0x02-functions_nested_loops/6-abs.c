9f5100ecdc425dde9094564bae029ca0d91909b1
MrFerhan <ferhkem@gmail.com>
2022-04-13 16:25:26 -0700

    task 6 absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value
+ *
+ * @n: the int to be checked
+ * Return: the value
+ */
+
+int _abs(int)
+{
+	int r;
+
+	if (r > 0)
+	{
+		return (r);
+	}
+	else if (r < 0)
+	{
+		return (-r);
+	}
+}

3990b039db7bf36d6ca6925210674b7674c95dd1
MrFerhan <ferhkem@gmail.com>
2022-04-18 13:26:23 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98(int *n) - main function to print 98
+ *
+ * @n: int var to be inpute
+ * Return: return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+

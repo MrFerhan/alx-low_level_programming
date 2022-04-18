25a965717c3b8e386cd3fc6de9f990127895e907
MrFerhan <ferhkem@gmail.com>
2022-04-18 15:07:30 -0700

    task 2 length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ * Return: return 0
+ */
+
+int _strlen(char *s)
+{
+	int lengthofthestring;
+
+	lengthofthestring = _strlen(char *s);
+}
+

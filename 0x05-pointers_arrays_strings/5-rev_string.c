a7e6b9d9cbf7ef01826765b80752beb4b848380e
MrFerhan <ferhkem@gmail.com>
2022-04-19 15:48:03 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int rev_i = 0;
+	char rev_i_strng;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i > 0)
+	{
+		rev_i_strng = s[i];
+		s[i] = s[rev_i];
+		s[rev_i] = rev_i_strng;
+		i--;
+		rev_i++;
+	}
+}

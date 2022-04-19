ddaeed9da699e0e806a654cdc7a4537b6d9fd817
MrFerhan <ferhkem@gmail.com>
2022-04-19 15:16:06 -0700

    4

---
+++ 0x05-pointers_arrays_strings/2-strlenforPLD1.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ * Return: return 0
+ */
+
+int _strlen(char *s)
+{
+	int LOF;
+
+	for (LOF = 0; s[LOF] != '\0'; LOF++)
+	{
+	LOF++;
+	}
+return (LOF);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ */
+
+void print_rev(char *s);
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+	_putchar(s[i]);
+	i--;
+	}
+	_putchar('\n');
+}

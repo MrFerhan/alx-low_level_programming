f19bf5bde8ebc2daa55d025af65b4b6d305efc01
MrFerhan <ferhkem@gmail.com>
2022-04-18 18:21:51 -0700

    33

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ * Return: return 0
+ */
+
+void _puts(char *str)
+{
+	_putchar(str[]);
+	_putchar('\n');
+}

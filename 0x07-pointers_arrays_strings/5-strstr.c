54ad9ca75159a5faba621bd37661f500ee1006b9
MrFerhan <ferhkem@gmail.com>
2022-04-28 15:31:49 -0700

    bettyyyyyy

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,9 +23,9 @@ char *_strstr(char *haystack, char *needle)
 			k++;
 		}
 		if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
+		{
+			return (haystack + i);
+		}
 	}
 	return (0);
 }

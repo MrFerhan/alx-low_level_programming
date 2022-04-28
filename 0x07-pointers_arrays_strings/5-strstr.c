b1d04bc652adbf2b609796aa505ad9ce6a1b79c3
MrFerhan <ferhkem@gmail.com>
2022-04-28 13:42:47 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		j = 0;
-		k = 0;
+		k = i;
 		while (haystack[k] == needle[j] && needle[j] != '\0')
 		{
 			j++;

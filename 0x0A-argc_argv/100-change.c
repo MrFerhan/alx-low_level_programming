986653996d711454c7ad08cd3288de12754b0d26
MrFerhan <ferhkem@gmail.com>
2022-05-02 15:55:22 -0700

    bonus

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,28 +15,28 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-	cents = atoi (argv[1]);
+	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
 		coins++;
-		if((cents - 25) >= 0)
+		if ((cents - 25) >= 0)
 		{
 			cents -= 25;
 			continue;
 		}
-		if((cents - 10) >= 0)
+		if ((cents - 10) >= 0)
 		{
 			cents -= 10;
 			continue;
 		}
-		if((cents - 5) >= 0)
+		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-		if((cents - 2) >= 0)
+		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
 			continue;

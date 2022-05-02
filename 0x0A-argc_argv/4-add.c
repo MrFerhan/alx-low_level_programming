544eded2faef07cedddb18bbc57bc8816dff7736
MrFerhan <ferhkem@gmail.com>
2022-05-01 17:32:36 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of the program
+ * @argc: numbe of arguments in the program
+ * @argv: array pointed to the argument held
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+	
+	if (argc <= 1)
+		{
+		printf("0\n");
+		}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+			{
+			sum += atoi(argv[i]);
+			printf("%d\n", sum);
+			}
+			else
+			{
+			printf("Error\n");
+			return (1);
+			}
+			}
+		}
+	}
+	return (0);
+}

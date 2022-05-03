7fb3dc1463a1281ab6592f46c0762c4dce68ab44
MrFerhan <ferhkem@gmail.com>
2022-05-03 16:31:05 -0700

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - brings to strings togther
+ * @s1: string 1
+ * @st: string 2
+ *
+ * Return: null or a pointer to nw_arr
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *nw_arr;
+	unsigned int i, j, k, limit;
+
+	if (s1 == NULL)
+		s1 == "";
+	if (s2 == NULL)
+		s2 == "";
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	nw_arr = malloc(sizeof(char) * (i + j + 1));
+
+	if (nw_arr == NULL)
+	{
+		free(nw_arr);
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+		nw_arr[k] = s1[k];
+	limit = j;
+	for (j =0; j <= limit; k++, j++)
+		nw_arr[k] = s2[j];
+
+	return (nw_arr);
+}

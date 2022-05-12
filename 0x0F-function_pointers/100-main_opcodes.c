f2107f5f9cd54650ba5f4c61f30f39cd695c0ef7
MrFerhan <ferhkem@gmail.com>
2022-05-12 14:32:37 -0700

    bonus

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcode of its own function
+ * @argc: size of args
+ * @argv: values of args
+ *
+ * Return: Error or result
+ */
+
+int main (int argc, char **argv)
+{
+	int i, byte;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	byte = atoi(argv[1]);
+	if (byte < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i <(byte - 1); i++)
+	{
+		if (byte != 0)
+			printf("%x", ((char *)main)[i]);
+	}
+	printf("%x", ((char *)main)[i]);
+	return (0);
+}

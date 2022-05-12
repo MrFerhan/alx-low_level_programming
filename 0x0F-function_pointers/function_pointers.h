8b8b59813aa5a34736beee422b94884204dd0234
MrFerhan <ferhkem@gmail.com>
2022-05-11 17:37:04 -0700

    2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 #include <stddef.h>
+
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

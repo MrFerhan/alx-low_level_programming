e0c47c24142a4688f85b60a6ac480ab620fe6a94
MrFerhan <101419637+MrFerhan@users.noreply.github.com>
2022-05-20 13:41:12 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
   *
   *Return: the number of nodes.
   */
-
+ 
 size_t print_list(const list_t *h)
 {
 	int count = 0;
@@ -15,6 +15,8 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
 		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;

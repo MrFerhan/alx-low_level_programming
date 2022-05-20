9fe376cf8fe04a421f70136ceeae45374f127d2e
MrFerhan <101419637+MrFerhan@users.noreply.github.com>
2022-05-20 16:32:39 -0700

    free lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+  *free_list - frees a list_t
+  *@head: pointer to head of list.
+  *
+  *Return: 0.
+  */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+i		head = head->next;
+		free(head);
+	}
+}

7f0beba0b9a3efeb69a1a0a42af36f547d33d86c
MrFerhan <101419637+MrFerhan@users.noreply.github.com>
2022-05-30 14:26:36 -0700

    1 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ * Return: return 0
+ */
+
+int _strlen(char *s)
+{
+	int LOF;
+	int i = 0;
+
+	for (LOF = 0; s[LOF] != '\0'; LOF++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
 * create_file - create file
 * @filename: file to be created
 * @text_content: content to write into the file.
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - main function to swqp
+ *
+ * @s: int var to be checked for tis length
+ * Return: return 0
+ */
+
+int _strlen(char *s)
+{
+	int LOF;
+	int i = 0;
+
+	for (LOF = 0; s[LOF] != '\0'; LOF++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
 * append_text_to_file - appends text at the end of a file.
 * @filename: file to append.
 * @text_content: info to append into the file.
